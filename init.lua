11c9fe8fd881f46981e7e48a1452d0a0c3277c5d
Maciej Ziaja <maciejzjg@gmail.com>
2022-04-15 18:41:39 +0200

    Diagnostics bindings for nvim lsp

--- init.lua
+++ init.lua
@@ -87,6 +87,14 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 local lspconfig = require("lspconfig")
+
+-- Diagnostics bindings
+local opts = { noremap=true, silent=true }
+vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
 -- Register servers
 local servers = {"clangd", "pylsp"}
 for _, lsp in ipairs(servers) do
