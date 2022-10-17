c91fc10dc82fee7f050d4808524af6b141bf0c69
Maciej Ziaja <maciejzjg@gmail.com>
2022-10-17 13:17:58 +0200

    Update cmp nvim lsp settings

--- init.lua
+++ init.lua
@@ -90,7 +90,7 @@ require("spellsitter").setup()
 
 -- LSP
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local lspconfig = require("lspconfig")
 
