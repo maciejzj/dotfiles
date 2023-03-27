3e6af5b965c592a0a70bdca05db4dc1cf9d38598
Maciej Ziaja <maciejzjg@gmail.com>
2023-03-27 19:49:07 +0200

    Very minor nvim conf cleanups

--- init.lua
+++ init.lua
@@ -89,7 +89,7 @@ require("packer").startup{
   config = {
     display = {
       open_fn = function()
-        return require('packer.util').float{border = 'single'}
+        return require("packer.util").float{border = "rounded"}
       end
     }
   }
