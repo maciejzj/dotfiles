6542457e362df6aafb12f84722fc9192731c145c
Maciej Ziaja <maciejzjg@gmail.com>
2023-04-04 11:18:39 +0200

    Minor keymap cleanups

--- init.lua
+++ init.lua
@@ -109,15 +109,19 @@ wk.register{
   ["<leader>p"] = {name = "plugins"}
 }
 
+-- General nvim functionalities keymaps
+
 vim.keymap.set("n", "<leader>e", ":Explore<CR>", defopts("File explorer"))
 vim.keymap.set("n", "<leader>ce", ":edit ~/.config/nvim/init.lua<CR>", defopts("Edit editor config"))
 vim.keymap.set("n", "<leader>cr", ":source ~/.config/nvim/init.lua<CR>", defopts("Reload editor config"))
-vim.keymap.set("n", "<leader>n", ":nohlsearch<CR>", defopts("Disable search highlight"))
+vim.keymap.set("n", "<leader>n", ":set hlsearch!<CR>", defopts("Toggle search highlight"))
+vim.keymap.set("n", "<leader>ts", ":set spell!<CR>", defopts("Toggle spellchecking"))
+
+-- Plugin management keymaps
 
 vim.keymap.set("n", "<leader>ps", ":PackerStatus<CR>", defopts("Plugins status"))
 vim.keymap.set("n", "<leader>pu", ":PackerUpdate<CR>", defopts("Update plugins"))
 vim.keymap.set("n", "<leader>pi", ":PackerInstall<CR>", defopts("Install plugins"))
-
 
 -- Automatic indentation (if indent is detected will override the defaults)
 require("guess-indent").setup()
@@ -174,6 +178,7 @@ vim.keymap.set("n", "<leader>fc", tb.commands, defopts("Find command"))
 vim.keymap.set("n", "<leader>fh", tb.help_tags, defopts("Search help"))
 vim.keymap.set("n", "<leader>fr", tb.command_history, defopts("Search command history"))
 vim.keymap.set("n", "<leader>fb", tb.buffers, defopts("Find buffer"))
+vim.keymap.set("n", "<leader>fk", tb.keymaps, defopts("Find keymap"))
 
 local is_repo = vim.fn.system("git rev-parse --is-inside-work-tree")
 if vim.v.shell_error == 0 then
@@ -271,7 +276,7 @@ for _, lsp in ipairs(servers) do
           vim.diagnostic.enable()
         end
       end
-      vim.keymap.set("n", "<leader>tq", toggle_diagnostics, defopts("Toogle diagnostics"))
+      vim.keymap.set("n", "<leader>tq", toggle_diagnostics, defopts("Toggle diagnostics"))
     end,
     capabilities = capabilities
   }
@@ -364,17 +369,17 @@ require("gitsigns").setup {
     vim.keymap.set("n", "<leader>hR", gs.reset_buffer, defopts("Restore buffer"))
     vim.keymap.set("n", "<leader>hp", gs.preview_hunk, defopts("Preview hunk"))
     vim.keymap.set("n", "<leader>hb", gs.blame_line, defopts("Blame line"))
-    vim.keymap.set("n", "<leader>tb", gs.toggle_current_line_blame, defopts("Toogle current line blame"))
+    vim.keymap.set("n", "<leader>tb", gs.toggle_current_line_blame, defopts("Toggle current line blame"))
     vim.keymap.set("n", "<leader>gd", gs.diffthis, defopts("Diff buffer"))
     vim.keymap.set("n", "<leader>gD", function() gs.diffthis("~") end, defopts("Diff buffer (with staged)"))
-    vim.keymap.set("n", "<leader>td", gs.toggle_deleted, defopts("Toogle show deleted"))
+    vim.keymap.set("n", "<leader>td", gs.toggle_deleted, defopts("Toggle show deleted"))
   end
 }
 
 -- Colorscheme
 vim.cmd([[colorscheme onedark]])
 
---- Fixes and workarounds
+-- Fixes and workarounds
 
 -- Disable error highlighting for markdown
 vim.api.nvim_exec([[ highlight link markdownError None ]], false)
