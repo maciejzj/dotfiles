e02cc99709d52a5273c4e57fec3a71f1ffcef8ba
Maciej Ziaja <maciejzjg@gmail.com>
2023-12-26 22:25:27 +0100

    Update IndentBlankline refresh on folds

--- configs/nvim_init.lua
+++ configs/nvim_init.lua
@@ -633,6 +633,6 @@ for _, keymap in pairs({
   "zo", "zO", "zc", "zC", "za", "zA", "zv", "zx", "zX", "zm", "zM", "zr", "zR",
 }) do
   vim.api.nvim_set_keymap(
-    "n", keymap, keymap .. "<CMD>IndentBlanklineRefresh<CR>", { noremap = true, silent = true }
+    "n", keymap, keymap .. ":lua require('ibl').refresh()<CR>", { noremap = true, silent = true }
   )
 end
