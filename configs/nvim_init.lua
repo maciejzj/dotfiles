3505d50bcbf1c78a03e252c15fc8e4206af6baf8
Maciej Ziaja <maciejzjg@gmail.com>
2024-01-25 11:52:11 +0100

    Disable regex highlight in treesitter

--- configs/nvim_init.lua
+++ configs/nvim_init.lua
@@ -147,6 +147,7 @@ require("nvim-treesitter.configs").setup({
   auto_install = true,
   highlight = {
     enable = true,
+    additional_vim_regex_highlighting = false,
   },
   indent = {
     enable = true
