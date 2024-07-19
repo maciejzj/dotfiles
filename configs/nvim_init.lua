e317dc27bd5568bba66dd612f287b88cf5dfd62b
Maciej Ziaja <maciejzjg@gmail.com>
2024-07-19 15:12:03 +0200

    Ignore vimgrep in cmp

--- configs/nvim_init.lua
+++ configs/nvim_init.lua
@@ -379,7 +379,7 @@ cmp.setup.cmdline(":", {
   sources = cmp.config.sources({
     { name = "path" },
   }, {
-    { name = "cmdline" },
+    { name = "cmdline", option = { ignore_cmds = { "!", "vimgrep" } } }
   }),
 })
 
