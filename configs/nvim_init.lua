af99a572f8100417c13d4168e7788301362e1265
Maciej Ziaja <maciejzjg@gmail.com>
2024-10-14 11:20:18 +0200

    Pin down lspconfig and load theme on install

--- configs/nvim_init.lua
+++ configs/nvim_init.lua
@@ -95,46 +95,52 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Plugins
 ---@format disable-next
-require("lazy").setup({
-  -- Language support package management
-  "williamboman/mason.nvim",
-  -- Help
-  "folke/which-key.nvim",
-  -- Treesitter
-  "nvim-treesitter/nvim-treesitter",
-  "nvim-treesitter/nvim-treesitter-textobjects",
-  "chrisgrieser/nvim-various-textobjs",
-  -- LSP
-  "neovim/nvim-lspconfig",
-  "williamboman/mason-lspconfig.nvim",
-  "nvimtools/none-ls.nvim",
-  "antosha417/nvim-lsp-file-operations",
-  "ray-x/lsp_signature.nvim",
-  -- Core functionalities
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-cmdline",
-  "hrsh7th/cmp-path",
-  "l3mon4d3/luasnip",
-  "nmac427/guess-indent.nvim",
-  "theprimeagen/refactoring.nvim",
-  -- Editor functionalities
-  "kylechui/nvim-surround",
-  "rrethy/vim-illuminate",
-  -- UI, visuals and tooling
-  "hiphish/rainbow-delimiters.nvim",
-  "stevearc/dressing.nvim",
-  { "nvim-neo-tree/neo-tree.nvim", dependencies = { "nvim-lua/plenary.nvim", "muniftanjim/nui.nvim" } },
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-  "lukas-reineke/indent-blankline.nvim",
-  { "catppuccin/nvim", priority = 1000 },
-  -- External tools integration
-  "lewis6991/gitsigns.nvim",
-  "folke/lazydev.nvim",
-  { "michaelb/sniprun", branch="master", build="sh install.sh" },
-})
+require("lazy").setup(
+  {
+    -- Language support package management
+    "williamboman/mason.nvim",
+    -- Help
+    "folke/which-key.nvim",
+    -- Treesitter
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "chrisgrieser/nvim-various-textobjs",
+    -- LSP
+    -- Pinned until https://github.com/folke/lazydev.nvim/issues/70 is resolved
+    { "neovim/nvim-lspconfig", tag = "v1.0.0" },
+    "williamboman/mason-lspconfig.nvim",
+    "nvimtools/none-ls.nvim",
+    "antosha417/nvim-lsp-file-operations",
+    "ray-x/lsp_signature.nvim",
+    -- Core functionalities
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-path",
+    "l3mon4d3/luasnip",
+    "nmac427/guess-indent.nvim",
+    "theprimeagen/refactoring.nvim",
+    -- Editor functionalities
+    "kylechui/nvim-surround",
+    "rrethy/vim-illuminate",
+    -- UI, visuals and tooling
+    "hiphish/rainbow-delimiters.nvim",
+    "stevearc/dressing.nvim",
+    { "nvim-neo-tree/neo-tree.nvim", dependencies = { "nvim-lua/plenary.nvim", "muniftanjim/nui.nvim" } },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+    "lukas-reineke/indent-blankline.nvim",
+    { "catppuccin/nvim", priority = 1000 },
+    -- External tools integration
+    "lewis6991/gitsigns.nvim",
+    "folke/lazydev.nvim",
+    { "michaelb/sniprun", branch="master", build="sh install.sh" },
+  },
+  {
+    install = { colorscheme = { 'catppuccin' } }
+  }
+)
 
 ----------✦ ❓ Help ❓ ✦----------
 
